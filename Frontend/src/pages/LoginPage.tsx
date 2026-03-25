fde968393767b94c8ef0ab6697f0d4c90ab4483e
aileenchy <seidlaileen@gmail.com>
2026-03-24 20:43:26 -0400

    Created LoginPage.tsx

---
+++ Frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,17 @@
+import PageTitle from "../components/PageTitle";
+import Login from "../components/LogIn";
+
+const LoginPage = () =>
+{
+    return
+    (
+        <div>
+            <PageTitle />
+            <Login  />
+
+        </div>
+    );
+
+};
+
+export default LoginPage;
